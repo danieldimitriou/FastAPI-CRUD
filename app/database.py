f2743024c086ec899934b8f391b90c161681cb12
danieldimitriou <danieldimitriou1@gmail.com>
2023-07-28 19:04:35 +0300

    uncomment code

--- app/database.py
+++ app/database.py
@@ -1,13 +1,14 @@
-# from beanie import init_beanie
-# import motor.motor_asyncio
-# from models import UserInDB
-#
-#
-# async def init_db():
-#     # db_name = "users"
-#     client = motor.motor_asyncio.AsyncIOMotorClient(
-#         "mongodb://localhost:27017/users"
-#     )
-#
-#     await init_beanie(database=client["user_db"], document_models=[UserInDB])
-#     print(client)
+import motor.motor_asyncio
+from beanie import init_beanie
+
+from models import UserInDB
+
+
+async def init_db():
+    # db_name = "users"
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        "mongodb://localhost:27017/users"
+    )
+
+    await init_beanie(database=client["user_db"], document_models=[UserInDB])
+    print(client)
--- app/models.py
+++ app/models.py
@@ -1,18 +1,19 @@
-# from uuid import UUID, uuid4
-# from pydantic import Field
-# from schemas import UserCreate
-# from beanie import Document, Indexed
-# from datetime import date
-#
-#
-# # Beanie Document (ODM) model
-# class UserInDB(Document, UserCreate):
-#     id: Indexed(UUID) = Field(default_factory=uuid4)
-#
-#     class Settings:
-#         # Set the collection name
-#         name = "users5"
-#         # Set an encoder for the date
-#         bson_encoders = {
-#             date: str
-#         }
+from beanie import Document, Indexed
+from datetime import date
+from pydantic import Field
+from uuid import UUID, uuid4
+
+from schemas import UserCreate
+
+
+# Beanie Document (ODM) model
+class UserInDB(Document, UserCreate):
+    id: Indexed(UUID) = Field(default_factory=uuid4)
+
+    class Settings:
+        # Set the collection name
+        name = "users5"
+        # Set an encoder for the date
+        bson_encoders = {
+            date: str
+        }
